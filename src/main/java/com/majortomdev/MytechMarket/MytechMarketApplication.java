222d1a680fc1f3cc862a6307191c4a83dec32d1c
Joseph Kinahan <kinahanj25@gmail.com>
2020-10-15 22:33:24 +0100

    sboot app w conn 2mysql

---
+++ src/main/java/com/majortomdev/MytechMarket/MytechMarketApplication.java
@@ -0,0 +1,13 @@
+package com.majortomdev.MytechMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytechMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytechMarketApplication.class, args);
+	}
+
+}
